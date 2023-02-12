e0d5438619051875c7d230a82b1dff23d0916a5e
Baljinnyam Dayan <baljinnyam.dayan@gmail.com>
2023-02-12 13:59:41 +0800

    i hate myself

--- operation_research/main.cpp
+++ operation_research/main.cpp
@@ -1,21 +1,30 @@
+#include <cstddef>
 #include <iostream>
 #include <queue>
 #include <string>
 #include <vector>
 using namespace std;
 
-double urvuu(double n) { return 1 / n; }
+double reverse(double n) { return 1 / n; }
 double column(double n, double cnt) { return n / cnt; }
-double row(double n, double cnt) { return -(n / cnt); }
+double rowc(double n, double cnt) { return -(n / cnt); }
 double other(double cnt, double b, double c, double d) {
   return ((cnt * b) - (d * c)) / cnt;
 }
-void copy(double rs[4][4], queue<double> temp) {
-  for (int i = 1; i < 4; i++) {
-    for (int j = 1; j < 4; j++) {
+void copy(vector<vector<double>> &rs, queue<double> temp) {
+  for (int i = 0; i < rs.size(); i++) {
+    for (int j = 0; j < rs[i].size(); j++) {
       rs[i][j] = temp.front();
       temp.pop();
     }
+  }
+}
+void printMatrix(int col, int row, vector<vector<double>> rs) {
+  for (int i = 0; i < col; i++) {
+    for (int j = 0; j < row; j++) {
+      cout << rs[i][j] << " ";
+    }
+    cout << endl;
   }
 }
 
@@ -25,24 +34,60 @@ int main() {
   vector<string> row;
   vector<string> col;
   cin >> n;
-  vector<vector<double>> matrix;
   for (int i = 0; i < n; i++) {
     row.push_back("x" + to_string(i));
   }
   cout << "heden ilerhiilel ?";
   int m;
   cin >> m;
+  vector<vector<double>> matrix(m, vector<double>(n));
   for (int i = 0; i < m; i++) {
     for (int j = 0; j <= n; j++) {
       if (j == n) {
         cout << "hariu : ";
-        cin >> col[i];
+        string temp;
+        cin >> temp;
+        col.push_back(temp);
       } else {
         cout << "x" << j << " = ";
         cin >> matrix[i][j];
       }
     }
   }
-
+  // ilerhiilliin toogoor bodolt hiigdne
+  for (int i = 0; i < m; i++) {
+    if (i == n) {
+      break;
+    }
+    string temp;
+    temp = col[i];
+    col[i] = row[i];
+    row[i] = temp;
+    queue<double> copies;
+    for (int j = 0; j < m; j++) {
+      for (int k = 0; k < n; k++) {
+        // bodolt is here
+        if (j == i && k == i) {
+          copies.push(reverse(matrix[j][k]));
+        } else if (j == i) {
+          copies.push(column(matrix[j][k], matrix[i][i]));
+        } else if (k == i) {
+          copies.push(rowc(matrix[j][k], matrix[i][i]));
+        } else {
+          copies.push(
+              other(matrix[i][i], matrix[j][k], matrix[j][i], matrix[i][k]));
+        }
+      }
+    }
+    copy(matrix, copies);
+    printMatrix(n, m, matrix);
+  }
+  for (int i = 0; i < m; i++) {
+    double result;
+    for (int j = 0; j < n; j++) {
+      result = result + matrix[i][j] * stod(row[j]);
+    }
+    cout << col[i] << " = " << result << endl;
+  }
   return 0;
 }
