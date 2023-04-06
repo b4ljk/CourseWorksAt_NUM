891613abf0f850c7f07d3c3bff733045050cbfe7
Baljinnyam <baljinnyam@Munkhbolds-MacBook-Pro.local>
2023-04-06 17:09:07 +0800

    uvicorn

--- operation_research/simplex/back.py
+++ operation_research/simplex/back.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 async def root():
-    return {"message": "ewewew World"}
+    return {"message": "Hello World ayooo broooo "}
+
+
+@app.post("/body/")
+async def create_item(request: Request):
+    body = await request.json()
+    return {"request_body": body}
+# uvicorn back:app --reload --host 0.0.0.0 --port 7965
