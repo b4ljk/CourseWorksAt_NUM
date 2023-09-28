2c2f5fcb55e64c1c76cd506ced817c0d127931d5
Baljinnyam Dayan <d.baljinnyam@mn.data-artist.com>
2023-09-28 19:31:05 +0800

    lab2

--- pattern_recognition/lab2/main.py
+++ pattern_recognition/lab2/main.py
@@ -43,3 +43,87 @@ for key, _intr in interplotation.items():
             f"{current_directory}/result/rose_{_intr}_{size}_{key}_difference.tif",
             difference_image,
         )
+
+# TASK 3 CONTRAST STRETCHING
+# create folder if it does not exists path: result/contrast_stretching
+if not os.path.exists(f"{current_directory}/result/contrast_stretching"):
+    os.makedirs(f"{current_directory}/result/contrast_stretching")
+
+img = cv2.imread("input/lab3photo.tif")
+
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+min_val, max_val, _, _ = cv2.minMaxLoc(gray)
+
+scale = (255 - 0) / (max_val - min_val)
+shift = 0 - (scale * min_val)
+
+stretched = cv2.convertScaleAbs(gray, alpha=scale, beta=shift)
+
+bit1 = cv2.bitwise_and(stretched, 0x80)
+
+
+
+cat = cv2.hconcat([gray, stretched, bit1])
+
+cv2.imwrite(f"{current_directory}/result/contrast_stretching/lab3photo.tif", cat)
+
+
+# TASK 4 GRAY-LEVEL SLICING
+if not os.path.exists(f"{current_directory}/result/gray_level_slicing"):
+    os.makedirs(f"{current_directory}/result/gray_level_slicing")
+
+img = cv2.imread('input/task4.tif')
+images = []
+
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+bit2 = cv2.bitwise_and(gray, 0xC0)
+
+gray = cv2.resize(gray, [400, 400])
+bit1 = cv2.resize(gray, [400, 400])
+bit2 = cv2.resize(gray, [400, 400])
+
+for i in range(bit1.shape[0]):
+    for j in range(bit1.shape[1]):
+        if (bit1[i][j] < 150 and bit1[i][j] > 70):
+            bit1[i][j] = 37
+        else:
+            bit1[i][j] = 230
+
+for i in range(bit2.shape[0]):
+    for j in range(bit2.shape[1]):
+        if (bit2[i][j] < 150 and bit2[i][j] > 70):
+            bit2[i][j] = 70
+        else:
+            bit2[i][j] = 180
+
+
+images.append(gray)
+images.append(bit1)
+images.append(bit2)
+
+cat = cv2.hconcat(images)
+cv2.imwrite(f"{current_directory}/result/gray_level_slicing/task4.tif", cat)
+
+# TASK 5 BIT-PLANE SLICING
+if not os.path.exists(f"{current_directory}/result/bitplaneslicing"):
+    os.makedirs(f"{current_directory}/result/bitplaneslicing")
+
+img = cv2.imread('input/Fig0314(a)(100-dollars).tif')
+img = cv2.resize(img, (380, 250))
+images = []
+
+for i in range(8):
+    j = np.bitwise_and(img, 1 << i)
+    j = np.uint8(j * 255)
+    images.append(j)
+
+row1 = images[:4]
+row2 = images[4:]
+
+row1_cat = cv2.hconcat(row1)
+row2_cat = cv2.hconcat(row2)
+
+final_image = cv2.vconcat([row1_cat, row2_cat])
+cv2.imwrite(f"{current_directory}/result/bitplaneslicing/task5.tif", final_image)
